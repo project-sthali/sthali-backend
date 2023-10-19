91b19257fe30bf37074ec627dfbc38f7d467514f
jhunu.fernandes <jhunu.fernandes@gmail.com>
2023-10-19 08:57:35 -0300

    wip

--- src/db.py
+++ src/db.py
@@ -4,19 +4,17 @@ from sthali_crud import Model, DB as Base
 
 
 class DB(Base):
-    """DB
+    """Sthali Backend DB main class.
     """
-    def create(self, resource: Model, *args, **kwargs) -> Model:
-        return {'id': 0, 'name': 'fernandes', 'height': 27}
+    # def create(self, resource: Model, *args, **kwargs) -> Model:
+    #     breakpoint()
+    #     return {'id': randint(1, 10), **resource}
 
     def read(self, resource_id: int, *args, **kwargs) -> Model:
-        return {'id': 0, 'name': 'fernandes', 'height': 27}
+        return {'id': resource_id, 'name': 'fernandes', 'height': 27}
 
-    def update(self, resource_id: int, resource: Model, *args, **kwargs) -> Model:
-        return {}
+    def upsert(self, resource_id: int, resource_obj: Model, *args, **kwargs) -> Model:
+        return {**resource_obj, 'id': resource_id}
 
-    def upsert(self, resource_id: int, resource: Model, *args, **kwargs) -> Model:
-        return {}
-
-    def delete(self, resource_id: int, *args, **kwargs) -> None:
-        return None
+    # def delete(self, resource_id: int, *args, **kwargs) -> None:
+    #     return None
