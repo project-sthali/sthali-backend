b6f61f810b17454b0911808de5e0ddde24ef927c
jhunu.araujo <jhunu.araujo@niara.tech>
2023-10-20 18:09:22 -0300

    wip

--- src/__init__.py
+++ src/__init__.py
@@ -1,28 +1,32 @@
-from sthali_crud import Field, SthaliCRUD, ResourceSpec
+from pydantic.dataclasses import dataclass
+from sthali_crud import FieldDefinition, SthaliCRUD, ResourceSpec
 from .db import DB
+
 
 _resource_spec = ResourceSpec(
     name='people',
     fields=[
-        # Field(
-        #     name='id',
-        #     type=int,
-        # ),
-        Field(
+        FieldDefinition(
             name='name',
             type=str,
         ),
-        Field(
+        FieldDefinition(
             name='height',
             type=int,
-            default=123,
-            allow_none=False,
+            has_default=True,
+            default_value=123,
+            allow_none=True,
+        ),
+        FieldDefinition(
+            name='age',
+            type=int,
         ),
     ]
 )
 
+
 _db = DB()
 _sthalicrud = SthaliCRUD(_resource_spec=_resource_spec, _db=_db)
-DB.replace_model(_sthalicrud.model)
+# DB.replace_model(_sthalicrud.model)
 
 fastapi = _sthalicrud.app
