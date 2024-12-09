6b4955c648b2cb20c7598253f44d850e6c201392
jhunu.fernandes <jhunu.fernandes@gmail.com>
2024-12-08 23:02:48 -0300

    wip

--- run.sh
+++ run.sh
@@ -7,7 +7,6 @@ if [ "$ENV" = 'DOCKER' ]; then
     exec docker run --name sthali-backend --rm -p 8000:80 sthali-backend
 elif [ "$ENV" = 'LOCAL' ]; then
     echo "Running local"
-    cd ./src/
     exec uvicorn run:app --host 0.0.0.0 --port 8000 --reload
 else
     echo "No ENV found, nothing to run!!!"
--- src/sthali_backend/__init__.py
+++ src/sthali_backend/__init__.py
@@ -1,6 +1,10 @@
 """This module provides ..."""
 
+import collections.abc
+import typing
+
 import pydantic
+from sthali_auth import Auth, AuthSpecification
 
 import sthali_crud
 
@@ -17,14 +21,39 @@ __all__ = [
 default_lifespan = sthali_crud.default_lifespan
 
 
-@pydantic.dataclasses.dataclass(kw_only=True)
+@pydantic.dataclasses.dataclass
 class AppSpecification(sthali_crud.AppSpecification):
     """Represents the specification of a SthaliBackend application."""
+    auth: typing.Annotated[AuthSpecification, pydantic.Field(default=None, description="The dependencies for the application"),]
 
     def __post_init__(self):
         self.title = "SthaliBackend"
         self.description = "A FastAPI package for implement services."
 
+
 class SthaliBackend(sthali_crud.SthaliCRUD):
-    def __init__(self, app_spec: AppSpecification) -> None:
-        super().__init__(app_spec)
+    """A class to initialize and configure a FastAPI application with {...}.
+
+    Args:
+        app_spec (AppSpecification): The specification of the application, including title, description, summary,
+            version, dependencies, and resources.
+        lifespan (collections.abc.Callable[..., typing.Any]): The lifespan of the application.
+            Defaults to default_lifespan.
+    """
+
+    def __init__(
+        self, app_spec: AppSpecification, lifespan: collections.abc.Callable[..., typing.Any] = default_lifespan
+    ) -> None:
+        """Initializes the SthaliBackend instance.
+
+        Args:
+            app_spec (AppSpecification): The specification of the application, including title, description, summary,
+                version, dependencies, and resources.
+            lifespan (collections.abc.Callable[..., typing.Any]): The lifespan of the application.
+                Defaults to default_lifespan.
+        """
+        auth = Auth(app_spec.auth)
+        auth_dependency = auth.dependency
+        app_spec.dependencies.append(auth_dependency)
+
+        super().__init__(app_spec, lifespan)
